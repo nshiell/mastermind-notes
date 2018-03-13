c4920da55bf7462bbdaf2c0e925e11d981a431c9
Nicholas Shiell <nicholas@nshiell.com>
2018-03-13 18:51:37 +0000

    API

---
+++ www/api.php
@@ -0,0 +1,56 @@
+<?php
+
+use Doctrine\MongoDB\Connection;
+use Doctrine\ODM\MongoDB\Configuration;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
+
+require_once (__DIR__ . '/../vendor/autoload.php');
+
+$dotenv = new \Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv->load();
+
+AnnotationDriver::registerAnnotationClasses();
+
+$config = new Configuration();
+$config->setProxyDir(__DIR__ . '/../Proxies');
+$config->setProxyNamespace('Proxies');
+$config->setHydratorDir(__DIR__ . '/../Hydrators');
+$config->setHydratorNamespace('Hydrators');
+$config->setDefaultDB('mastermindNotes');
+$config->setMetadataDriverImpl(AnnotationDriver::create(__DIR__ . '/../Entity'));
+
+$options = [];
+$username = getenv('DATABASE_USERNAME');
+if ($username) {
+    $options['username'] = $username;
+}
+$password = getenv('DATABASE_PASSWORD');
+if ($password) {
+    $options['password'] = $password;
+}
+
+$host = getenv('DATABASE_HOST');
+if (!$host) {
+    $host = 'localhost';
+}
+
+$port = (int) getenv('DATABASE_PORT');
+if (!$port) {
+    $port = 27017;
+}
+
+$dm = DocumentManager::create(new Connection('mongodb://' . $host . ':' . $port, $options), $config);
+
+$notes = $dm->createQueryBuilder(\NShiell\MastermindNotes\Entity\Note::class)
+    ->getQuery()
+    ->execute();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $note = new \NShiell\MastermindNotes\Entity\Note;
+    $note->body = $_POST['body'];
+    $dm->persist($note);
+    $dm->flush();
+}
+
+echo json_encode(array_values($notes->toArray()));
