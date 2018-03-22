03ed172e08974533084c1946cab9ac15b4dd89fe
Nicholas Shiell <nicholas@nshiell.com>
2018-03-22 16:41:51 +0000

    allow nohup in docker

--- docker-run.sh
+++ docker-run.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # usage docker-run.sh
-# usage docker-run.sh [apachePort] [mongoPort]
+# usage docker-run.sh [[[apachePort] mongoPort] nohup]
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ -z $1 ]; then apachePort=8080; else apachePort=$1; fi
 if [ -z $2 ]; then mongoPort=27017; else mongoPort=$2; fi
 
-
 PURPLE='\033[0;35m'
-
 WHITE='\033[1;37m'
 RESET='\033[0m'
 
@@ -33,10 +31,28 @@ echo -e "${PURPLE}Running Mastermind Notes
     HTTP    on port: ${WHITE}$apachePort${PURPLE} to ${WHITE}127.0.0.1${PURPLE} only
     MongoDb on port: ${WHITE}$mongoPort${PURPLE} to ${WHITE}127.0.0.1${PURPLE} only${RESET}"
 
-docker run                        \
-    --rm                          \
-    -it                           \
-    -p 127.0.0.1:$apachePort:80   \
-    -p 127.0.0.1:$mongoPort:27017 \
-    -v "$DIR:/app/"               \
-    nshiell/linux-php7.2-mongo:latest
+
+if [ "$3" == "nohup" ]; then
+    echo "${PURPLE}Running container nohup.."
+    echo "use docker stop mastermind-notes${RESET}"
+
+    nohup docker run                  \
+        --rm                          \
+        -i                            \
+        -p 127.0.0.1:$apachePort:80   \
+        -p 127.0.0.1:$mongoPort:27017 \
+        -v "$DIR:/app/"               \
+        --name mastermind-notes       \
+        nshiell/linux-php7.2-mongo:latest &
+else
+    echo "${PURPLE}Running container interactive...${RESET}"
+
+    docker run                        \
+        --rm                          \
+        -it                           \
+        -p 127.0.0.1:$apachePort:80   \
+        -p 127.0.0.1:$mongoPort:27017 \
+        -v "$DIR:/app/"               \
+        --name mastermind-notes       \
+        nshiell/linux-php7.2-mongo:latest
+fi
